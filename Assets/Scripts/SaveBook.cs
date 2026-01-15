28cfb586902a3fd8083fcd1ebaafaf0cdd4f3ae9
LouLFBV <lou.lefebvre@ynov.com>
2026-01-15 21:30:26 +0100

    Livre de sauvegarde ajouté

---
+++ Assets/Scripts/SaveBook.cs
@@ -0,0 +1,59 @@
+using UnityEngine;
+
+public class SaveBook : InteractableBase
+{
+    [Header("Visual Floating")]
+    public bool enableFloating = false;
+    [SerializeField] private float floatAmplitude = 0.25f;
+    [SerializeField] private float floatSpeed = 2f;
+    [SerializeField] private float rotationSpeed = 60f;
+
+    private Vector3 startPosition;
+    private float timeOffset;
+    private Rigidbody rb;
+
+    [Header("Save Panel")]
+    private GameObject savePanel; // Reference to the save panel UI
+    private bool isSavePanelOpen = false;
+
+    private void Awake()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    private void Start()
+    {
+        startPosition = transform.position;
+        timeOffset = Random.Range(0f, 100f);
+
+        if (rb != null)
+        {
+            rb.isKinematic = enableFloating;
+            rb.useGravity = !enableFloating;
+        }
+    }
+
+    private void Update()
+    {
+        if (!enableFloating)
+            return;
+
+        transform.Rotate(Vector3.up * rotationSpeed * Time.deltaTime, Space.World);
+
+        float newY = startPosition.y + Mathf.Sin((Time.time + timeOffset) * floatSpeed) * floatAmplitude;
+        transform.position = new Vector3(transform.position.x, newY, transform.position.z);
+    }
+    public override void OnInteract(PlayerInteractor player)
+    {
+        OpenPanel();
+    }
+
+    private void OpenPanel()
+    {
+        if (savePanel != null && !isSavePanelOpen)
+        {
+            savePanel.SetActive(true);
+            isSavePanelOpen = true;
+        }
+    }
+}
