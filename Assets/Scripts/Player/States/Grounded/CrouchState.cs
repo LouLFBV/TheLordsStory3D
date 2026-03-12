f7494aabc7be754e2f08287c7e25dbf117d27588
LouLFBV <lou.lefebvre@ynov.com>
2026-03-12 09:21:18 +0100

    Crouch en cours d'ajout

--- Assets/Scripts/Player/States/Grounded/CrouchState.cs
+++ Assets/Scripts/Player/States/Grounded/CrouchState.cs
@@ -1,6 +1,15 @@
 public class CrouchState : GroundedState
 {
-    public CrouchState(PlayerController player) : base(player)
+    public CrouchState(PlayerController player) : base(player){}
+
+    public override void Enter()
     {
+        base.Enter();
+        player.Animator.SetBool("isCrouching", true);
+        //player.Motor.SetCrouch(true);
+    }
+    public override void Exit() { base.Exit();
+        player.Animator.SetBool("isCrouching", false);
+       // player.Motor.SetCrouch(false);
     }
 }
